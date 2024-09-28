aba6a14e5c99bdf845b9eb54e0042daba205983a
Devendra Selokar <selokardevendra05@gmail.com>
2024-09-28 23:15:01 +0530

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -8,15 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-// app.use(cors({
-//   origin: '*',
-//   methods: 'GET,POST,PUT,DELETE',
-//   allowedHeaders: 'Content-Type'
-// }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
-  next();
-});
+app.use(cors());
 
 
 
