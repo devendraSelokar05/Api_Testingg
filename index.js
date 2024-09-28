094297309da7d198025d0568a6438c8a87e60345
Devendra Selokar <selokardevendra05@gmail.com>
2024-09-28 22:58:07 +0530

    added some code

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ app.use(cors({
   origin: '*',
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type'
-}))
+}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+  next();
+});
+
 
 
 
