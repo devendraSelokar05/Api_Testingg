94c166aed5eba47154471d8241aa65ba58b3bf10
Devendra Selokar <selokardevendra05@gmail.com>
2024-09-29 12:25:01 +0530

    added headers

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.use(cors({
   allowedHeaders: 'Content-Type'
 }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');  // Allow requests from any origin
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');  // Allow specific methods
+  res.header('Access-Control-Allow-Headers', 'Content-Type');  // Allow specific headers
+  next();
+});
+
 
 
 // Movies data (for your movie API)
