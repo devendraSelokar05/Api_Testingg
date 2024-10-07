4584a9475d8aa204c17a90c0f0f51cb7bdc84a93
Devendra Selokar <selokardevendra05@gmail.com>
2024-10-07 14:27:56 +0530

    video trailer is added on playbutton

--- index.js
+++ index.js
@@ -2778,7 +2778,7 @@ let movies = [
     "imdbRating": 7.6,
     "imdbVotes": "17k",
     "backdropImage": "https://images.justwatch.com/backdrop/198570000/s1440/sanam-teri-kasam.webp/sanam-teri-kasam.webp",
-    "posterImage": "https://m.media-amazon.com/images/M/MV5BOWZkZjhjNzktMWY5NC00NGI0LTk1OWItZGIwNDk5MDZhYWExXkEyXkFqcGc@._V1_.jpg",
+    "posterImage": "https://www.filmibeat.com/wimg/mobi/2015/12/sanam-teri-kasam_145076808810.jpg",
     "duration": "2h 34min",
     "highestRank": "07",
     "top10Days": "3 Days",
